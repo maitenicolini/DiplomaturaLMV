fa2a92b7da268a40e5a051b25bdd82cdff0cd36f
Maite Nicolini <maitenicol1@hotmail.com>
2023-05-24 08:30:56 -0300

    ejemplo 3

---
+++ clase 16/ejemplo2.js
@@ -0,0 +1,7 @@
+const link = document.getElementById('link')
+
+link.addEventListener('click', function(e){
+
+    e.preventDefault();
+    alert('quisiste ir a: '+ e.currentTarget.href )
+})
---
+++ clase 16/ejemplo3.js
@@ -0,0 +1,17 @@
+const formulario = document.getElementById('formulario');
+
+formulario.addEventListener('submit' , function(e){
+
+    e.preventDefault();
+
+    if(e.currentTarget.nombre.value == ''){
+        alert('El campo nombre es requerido')
+        return;
+    }
+
+    if(e.currentTarget.mensaje.value == ''){
+        alert('El campo mensaje es requerido')
+        return;
+    }
+    alert(`Gracias ${e.currentTarget.nombre.value} por enviarnos el siguiente mensaje: ${e.currentTarget.mensaje.value}`)
+})
